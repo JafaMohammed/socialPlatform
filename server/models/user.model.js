22a200ee0570e4c4da920d7fdd871a500da959bc
Jaafar Mohammed <jmohsa@gmail.com>
2021-02-02 18:15:44 +0300

    implemented follow, like, comment, and post

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -30,7 +30,9 @@ const UserSchema = new mongoose.Schema({
   photo: {
     data: Buffer,
     contentType: String
-  }
+  },
+  following: [{type: mongoose.Schema.ObjectId, ref: 'User'}],
+  followers: [{type: mongoose.Schema.ObjectId, ref: 'User'}]
 })
 
 UserSchema
@@ -45,8 +47,8 @@ UserSchema
   })
 
 UserSchema.path('hashed_password').validate(function(v) {
-  if (this._password && this._password.length < 8) {
-    this.invalidate('password', 'Password must be at least 8 characters.')
+  if (this._password && this._password.length < 6) {
+    this.invalidate('password', 'Password must be at least 6 characters.')
   }
   if (this.isNew && !this._password) {
     this.invalidate('password', 'Password is required')
